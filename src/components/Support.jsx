258b1c5298b1023bdcba14b002ab1fc235a9af52
Iva Karadjova <45530696+KodeIva@users.noreply.github.com>
2023-11-03 15:21:50 +0000

    Added counter code

---
+++ src/components/Support.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+const pageTitle = document.title
+
+export default () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    
+    count &&  (document.title = `${pageTitle}--${count}`)
+    
+  })
+
+  return (
+    <button 
+      className="outline" 
+      onClick={() => setCount(count + 1)}
+    >
+      {count === 0 ? 'Click to support' : `Supported ${count} times`}
+    </button>
+  )
+}
